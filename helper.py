1737823272746866e93ea3c93f32c9ead8a96d2e
alex4men <ezh25pr@hotmail.com>
2019-09-07 02:40:33 +0300

    Fixed non-reshape mod

--- helper.py
+++ helper.py
@@ -133,13 +133,12 @@ def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape)
 	"""
 	for image_file in glob(os.path.join(data_folder, 'image_2', '*.png')):
 		image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
-
 		# Run inference
 		im_softmax = sess.run(
 			[tf.nn.softmax(logits)],
 			{keep_prob: 1.0, image_pl: [image]})
 		# Splice out second column (road), reshape output back to image_shape
-		im_softmax = im_softmax[0][:, :, 1]
+		im_softmax = im_softmax[0][0][:, :, 1]
 		# If road softmax > 0.5, prediction is road
 		segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
 		# Create mask based on segmentation to apply to original image
